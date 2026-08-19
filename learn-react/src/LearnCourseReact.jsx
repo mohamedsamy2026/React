b4309115704442e48e3dc9941c26d2c14c8f2682
mohamed samy <mohamedeng747@gmil.com>
2026-08-19 17:12:24 +0300

    mohamed

--- learn-react/src/LearnCourseReact.jsx
+++ learn-react/src/LearnCourseReact.jsx
@@ -1,3 +1,5 @@
+import "./LearnCourseReact.css"
+
 export default function LearnCourseReact() {
   let x = 10;
 
@@ -6,20 +8,35 @@ export default function LearnCourseReact() {
     age: 17,
     email: "mohamedeng747@gmail.com",
   };
+
+  let style = {
+    backgroundColor: "green",
+    color:"white",
+    fontWeight: "bold",
+    borderRadius: "10px",
+    padding: "10px 20px",
+    cursor:"pointer",
+    border:"none"
+  }
   return (
     <div>
       <h2>
-        {Say()} {parson.name}
+        Hello {parson.name}
       </h2>
-      <h3>
-        MY Age {parson.age} Learn React JS expertise {x} Years
+      <h3 style={style}>e {x}
+        MY Age {parson.age} Learn React JS expertis Years
       </h3>
 
+      
       <h2>From Tarmez</h2>
+
+      <button className={parson.age >= 17 ?"active" : "noactive"}>Check Name</button>
+
+      <button style={style}  onClick={Say}>Click Aleart</button>
     </div>
   );
 }
 
 function Say() {
-  return "Hello ";
+  return alert("React JS");
 }
