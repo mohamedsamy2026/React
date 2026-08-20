622917a803581f18ae75f303beb7016031e01c2c
mohamed samy <mohamedeng747@gmil.com>
2026-08-20 16:14:07 +0300

    mohamed

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
-import LearnCourseReact from "./LearnCourseReact"
+// import LearnCourseReact from "./LearnCourseReact"
+import Props from "./Props";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,7 +22,6 @@ function App() {
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
-          <LearnCourseReact />
         </div>
         <button
           type="button"
@@ -30,6 +30,55 @@ function App() {
         >
           Count is {count}
         </button>
+
+        <Props
+          name="Mohamed"
+          email="mohameeng747@gmail.com"
+          age="17"
+          content="1"
+        >
+          <h2
+            style={{
+              color: "white",
+              backgroundColor: "green",
+              padding: "20px",
+              borderRadius: "10px",
+            }}
+          >
+            Heelo World 1
+          </h2>
+        </Props>
+
+        <Props name="Ahmed" email="ahmedeng888@gmail.com" age="16" content="2">
+          <h2
+            style={{
+              color: "white",
+              backgroundColor: "orange",
+              padding: "20px",
+              borderRadius: "10px",
+            }}
+          >
+            Heelo World 2
+          </h2>
+        </Props>
+
+        <Props
+          name="rodina"
+          email="rodinaeng123@gmail.com"
+          age="10"
+          content="3"
+        >
+          <h2
+            style={{
+              color: "white",
+              backgroundColor: "blue",
+              padding: "20px",
+              borderRadius: "10px",
+            }}
+          >
+            Heelo World 3
+          </h2>
+        </Props>
       </section>
 
       <div className="ticks"></div>
@@ -120,7 +169,5 @@ function App() {
     </>
   );
 }
-
-
 
 export default App;
---
+++ learn-react/src/Props.jsx
@@ -0,0 +1,14 @@
+export default function Props({name,email,age,children="No Content"}) {
+
+  return (
+    <>
+      <div style={{backgroundColor:"white", color:"black",borderRadius:"10px",padding:"10px",margin:"20px",fontWeight:"bold",fontSize:"20px",cursor:"pointer"}}>
+          <h2>{name}</h2>
+          <h3>{email}</h3>
+          <h3>{age}</h3>
+          <hr />
+          <p>{children}</p>
+      </div>
+    </>
+  );
+}
