736467c3b9b45ea1b97c0d68bcc0e0febe34ddde
mohamed samy <mohamedeng747@gmil.com>
2026-08-23 00:29:10 +0300

    mohamed

--- learn-react/src/UseStaute.jsx
+++ learn-react/src/UseStaute.jsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 
 export default function UseState() {
-  const [formInputs, setformInputs] = useState({ Name: "", email: "", age:"" });
+  const [formInputs, setformInputs] = useState({
+    Name: "",
+    email: "",
+    age: "",
+    message: "",
+    checked: false,
+  });
 
+  function message(event) {
+    setformInputs({ ...formInputs, message: event.target.value });
+  }
   return (
     <>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          console.log(formInputs.Name,formInputs.email,formInputs.age)
+          console.log(formInputs.Name, formInputs.email, formInputs.age);
         }}
       >
-
         <div>
           <label>Name</label>
           <input
@@ -19,7 +27,7 @@ export default function UseState() {
             placeholder="Please Enter The Name"
             value={formInputs.Name}
             onChange={(event) => {
-              setformInputs({...formInputs,Name:event.target.value});
+              setformInputs({ ...formInputs, Name: event.target.value });
             }}
           />
         </div>
@@ -31,7 +39,7 @@ export default function UseState() {
             placeholder="Please Enter The Email"
             value={formInputs.email}
             onChange={(event) => {
-              setformInputs({...formInputs,email:event.target.value});
+              setformInputs({ ...formInputs, email: event.target.value });
             }}
           />
         </div>
@@ -43,9 +51,31 @@ export default function UseState() {
             placeholder="Please Enter The Age"
             value={formInputs.age}
             onChange={(event) => {
-              setformInputs({...formInputs,age:event.target.value});
+              setformInputs({ ...formInputs, age: event.target.value });
             }}
           />
+        </div>
+
+        <div>
+          <textarea
+            rows="10"
+            cols="40"
+            placeholder="Enter The Message"
+            value={formInputs.message}
+            onChange={message}
+          ></textarea>
+        </div>
+
+        <div className="div">
+          <input
+            type="checkbox"
+            className="check"
+            checked={formInputs.checked}
+            onChange={(event) => {
+              setformInputs({...formInputs,checked:event.target.checked})
+            }}
+          />
+          <h4>هل انت فوق السن 17</h4>
         </div>
 
         <button className="btn">Submit</button>
