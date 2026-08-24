e36f34b25f8f9b971d2af7585855fc8e8eb61c50
mohamed samy <mohamedeng747@gmil.com>
2026-08-24 12:51:34 +0300

    mohamed

---
+++ Learn-projects-react/bank loan/vite.config.js
@@ -0,0 +1,13 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+  server:{
+      open:true,
+  }
+})
