57b517877492fe75a236118b3445a3ca0972d4ae
mohamed samy <mohamedeng747@gmil.com>
2026-08-24 12:31:48 +0300

    mohamed

--- Learn-projects-react/challenge-state-arrays-update/src/pedding.jsx
+++ Learn-projects-react/challenge-state-arrays-update/src/pedding.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function RequestTracker() {
   const [pending, setPending] = useState(0);
@@ -7,27 +7,25 @@ export default function RequestTracker() {
   async function handleClick() {
     setPending(pending + 1);
     await delay(3000);
-    setPending(pending - 1);
-    setCompleted(completed + 1);
+    setPending((count) => {
+      return count - 1;
+    });
+    setCompleted((count) => {
+      return count + 1;
+    });
   }
 
   return (
     <>
-      <h3>
-        Pending: {pending}
-      </h3>
-      <h3>
-        Completed: {completed}
-      </h3>
-      <button onClick={handleClick}>
-        Buy
-      </button>
+      <h3>Pending: {pending}</h3>
+      <h3>Completed: {completed}</h3>
+      <button onClick={handleClick}>Buy</button>
     </>
   );
 }
 
 function delay(ms) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
