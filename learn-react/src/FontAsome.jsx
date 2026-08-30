a1c587362db36ee92eeeefb7d07cf83c5fd67a05
mohamed samy <mohamedeng747@gmil.com>
2026-08-30 18:08:02 +0300

    mohamed

--- learn-react/src/FontAsome.jsx
+++ learn-react/src/FontAsome.jsx
@@ -1,11 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faThumbTack } from "@fortawesome/free-solid-svg-icons";
 
 function MyComponent() {
   return (
     <div className="flex justify-center items-center mt-10">
       {/* ثانياً: استخدام الأيقونة وتعديل حجمها ولونها بالـ className */}
-      <FontAwesomeIcon icon={faTrash} className="text-red-500 text-3xl" />
+      <div className="w-12 h-12 rounded-full bg-red-200 hover:bg-red-300 rounded-full flex items-center justify-center cursor-pointer duration-200">
+        <FontAwesomeIcon icon={faTrash} className="text-red-500 text-2xl" />
+      </div>
+      <FontAwesomeIcon icon={faThumbTack} />
     </div>
   );
 }
