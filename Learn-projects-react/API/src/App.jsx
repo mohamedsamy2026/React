5aff25cf4780f345aee6fa058bc6776d86d04365
mohamed samy <mohamedeng747@gmil.com>
2026-09-11 05:01:51 +0300

    mohamed

--- Learn-projects-react/API/src/App.jsx
+++ Learn-projects-react/API/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="font-father bg-blue-500 h-screen w-screen flex"></div>;
+  return <div className="font-father bg-blue-500 h-screen w-screen flex">
+    <div>
+      
+    </div>
+  </div>;
 }
 
 export default App;
