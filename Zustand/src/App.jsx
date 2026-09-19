3723446bdc51683840502153c62d9a1c967f2df9
mohamed samy <mohamedeng747@gmil.com>
2026-09-19 18:14:22 +0300

    mohamed

--- Zustand/src/App.jsx
+++ Zustand/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-2xl text-center bg-blue-500 py-3 px-4 rounded-3xl text-white font-bold">
+      <h1 className="text-2xl text-center bg-blue-500 py-3 px-4 text-white font-bold">
         Learn ZusTand
       </h1>
     </>
