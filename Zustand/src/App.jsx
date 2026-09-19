2f41c3bb33bcef33de41732ec2baaef70d60d68c
mohamed samy <mohamedeng747@gmil.com>
2026-09-19 18:14:02 +0300

    mohamed

---
+++ Zustand/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-center bg-blue-500 py-3 px-4 rounded-3xl text-white font-bold">
+        Learn ZusTand
+      </h1>
+    </>
+  );
+}
+
+export default App;
